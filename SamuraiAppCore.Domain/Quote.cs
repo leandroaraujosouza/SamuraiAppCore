0cf4b52a2de1d5a96d056a51ba406fe382011ab6
Leandro de Araújo Souza <leandroaraujosouza@outlook.com>
2017-07-16 17:28:15 -0300

    Add project files.

---
+++ SamuraiAppCore.Domain/Quote.cs
@@ -0,0 +1,11 @@
+﻿namespace SamuraiAppCore.Domain
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int SamuraiId { get; set; }
+
+        public Samurai Samurai { get; set; }
+    }
+}
